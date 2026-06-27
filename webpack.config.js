9742c9dbb2a43e596516b4e2f9a5cacd3aa2568a
Jimhasaburger <160654188+Jimhasaburger@users.noreply.github.com>
2026-06-27 11:52:56 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -186,7 +186,7 @@ module.exports = [
                 chunks: ['editor'],
                 template: 'src/playground/index.ejs',
                 filename: 'editor.html',
-                title: `${APP_NAME} - Run Scratch projects faster`,
+                title: `${APP_NAME} - Turbowarp with Online Projects!`,
                 isEditor: true,
                 ...htmlWebpackPluginCommon
             }),
@@ -194,14 +194,14 @@ module.exports = [
                 chunks: ['player'],
                 template: 'src/playground/index.ejs',
                 filename: 'index.html',
-                title: `${APP_NAME} - Run Scratch projects faster`,
+                title: `${APP_NAME} - Turbowarp with Online Projects!`,
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['fullscreen'],
                 template: 'src/playground/index.ejs',
                 filename: 'fullscreen.html',
-                title: `${APP_NAME} - Run Scratch projects faster`,
+                title: `${APP_NAME} - Turbowarp with Online Projects!`,
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
